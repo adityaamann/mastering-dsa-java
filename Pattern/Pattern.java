269bb2895185d1e9603d2ecb05011c3026476abe
Aditya Aman <161950598+adityaamann@users.noreply.github.com>
2025-06-14 13:52:46 +0530

    Add files via upload

---
+++ Pattern/Pattern.java
@@ -0,0 +1,77 @@
+public class Pattern {
+    public static void main(String[] args) {
+//        for(int i=1; i<=5; i++){
+//            for(int j=1; j<=5; j++){
+//                System.out.print(i+j+" ");
+//            }
+//            System.out.println();
+//        }
+
+//        for(int i=1; i<=5; i++){
+//            for(int j=1; j<=5; j++){
+//                System.out.print(i+j+" ");
+//            }
+//            System.out.println();
+//        }
+
+//        for(int i=1; i<=5; i++){
+//            for(int j=1; j<=5; j++){
+//                System.out.print(j+" ");
+//            }
+//            System.out.println();
+//        }
+
+//        for(int i=1; i<=5; i++){
+//            for(int j=1; j<=i; j++){
+//                System.out.print(j);
+//            }
+//            System.out.println();
+//        }
+
+//        int count = 0;
+//        for(int i=1; i<=5; i++){
+//            for(int j=1; j<=i; j++){
+//                count++;
+//                System.out.print(count+" ");
+//            }
+//            System.out.println();
+//        }
+
+//        for(int i=1; i<=5; i++){
+//            for(int j=1; j<=i; j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+
+//        for(int i=1; i<=5; i++){
+//            for(int j=1; j<=5-i+1; j++){
+//                System.out.print(j+" ");
+//            }
+//            System.out.println();
+//        }
+
+//        for(int i=1; i<=5; i++){
+//            for(int j=1; j<=5; j++){
+//                if(j>=i)
+//                System.out.print("* ");
+//                else
+//                    System.out.print("  ");
+//            }
+//            System.out.println();
+//        }
+
+
+//        for(int i=1; i<=5; i++){
+//            for(int j=1; j<=5; j++){
+//                if(j>=i)
+//                    System.out.print("* ");
+//                else
+//                    System.out.print("  ");
+//            }
+//            System.out.println();
+//        }
+
+        
+    }
+}
