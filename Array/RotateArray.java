965cac72d2a56904f2f044896e03f34fd537400e
Aditya Aman <161950598+adityaamann@users.noreply.github.com>
2025-06-15 22:03:26 +0530

    Add files via upload

---
+++ Array/RotateArray.java
@@ -0,0 +1,36 @@
+public class RotateArray {
+    public static void main(String[] args) {
+        // Left rotate an Array
+
+//        int[] arr = {4,5,2,3,1,7,17,12,9,12};
+//        for(int x:arr){
+//            System.out.print(x+" ");
+//        }
+//        System.out.println();
+//        int temp = arr[0];
+//        for(int i=1;i<arr.length;i++){
+//            arr[i-1] = arr[i];
+//        }
+//        arr[arr.length-1] = temp;
+//        for(int x:arr){
+//            System.out.print(x+" ");
+//        }
+
+        //Right Rotate an Array
+
+//        int[] arr = {4,5,2,3,1,7,17,12,9};
+//        for(int x:arr){
+//            System.out.print(x+" ");
+//        }
+//        System.out.println();
+//        int temp = arr[arr.length-1];
+//        for(int i=arr.length-2;i>=0;i--){
+//            arr[i+1] = arr[i];
+//        }
+//        arr[0] = temp;
+//        for(int x:arr){
+//            System.out.print(x+" ");
+//        }
+
+    }
+}
