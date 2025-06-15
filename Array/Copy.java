de26057d30dc269e6710d10b5baa3009f06e54b3
Aditya Aman <161950598+adityaamann@users.noreply.github.com>
2025-06-15 22:14:39 +0530

    Add files via upload

---
+++ Array/Copy.java
@@ -0,0 +1,25 @@
+public class Copy {
+    public static void main(String[] args) {
+        // original copy
+        
+//        int[] arr1 = {1,2,3,4,5};
+//        int[] arr2 = new int[arr1.length];
+//        for(int i=0;i<arr1.length;i++){
+//            arr2[i] = arr1[i];
+//        }
+//        for(int x : arr2){
+//            System.out.print(x+" ");
+//        }
+
+        // reversed copy
+
+//        int[] arr1 = {1,2,3,4,5};
+//        int[] arr2 = new int[arr1.length];
+//        for(int i=0;i<arr1.length;i++){
+//            arr2[arr1.length-1-i] = arr1[i];
+//        }
+//        for(int x : arr2){
+//            System.out.print(x+" ");
+//        }
+    }
+}
